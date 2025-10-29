6ac4ca3b36bdba027bae05c8e8a646699c4a0058
benjag27 <benjamin.gonzalez.132006@gmail.com>
2025-10-29 11:01:17 -0300

    base de de datos

--- src/main/resources/scheme.sql
+++ src/main/resources/scheme.sql
@@ -7,3 +7,28 @@ CREATE TABLE users (
     name TEXT NOT NULL UNIQUE,          -- Nombre de usuario (TEXT es el tipo de cadena recomendado para SQLite), con restricción UNIQUE
     password TEXT NOT NULL           -- Contraseña hasheada (TEXT es el tipo de cadena recomendado para SQLite)
 );
+
+create table Materia(
+    codigo  INT NOT NULL PRIMARY KEY,
+    nombre TEXT,
+    descripcion TEXT,
+    cod_plan INTEGER not null,
+    constraint  fk  Materia(cod_plan) references Plan_De_Estudios(cod_plan)
+);
+
+
+create table Plan_De_Estudios(
+   cod_plan INTEGER not null PRIMARY KEY,
+   año date,
+   vigencia INTEGER not null,
+   años_total INTEGER not null,
+   cantidad_materias_total INTEGER not null,
+
+);
+
+create table carrera(
+    codigo  INT NOT NULL PRIMARY KEY,
+    nombre TEXT,
+    descripcion TEXT,
+
+);
